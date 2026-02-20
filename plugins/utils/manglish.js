622cfe45665fe4d874d8fd2d94d2eb240cb55b12
zahid53402 <zahid.ali.00347@gmail.com>
2026-02-20 21:54:42 +0500

    Update manglish.js

--- plugins/utils/manglish.js
+++ plugins/utils/manglish.js
@@ -1,232 +1,51 @@
+/**
+ * ZAHID-KING-MD - Manglish & Malayalam Converter
+ * This module handles transliteration between Malayalam script and Roman text.
+ */
+
 const axios = require("axios");
-const translate = require("@vitalets/google-translate-api");
 
-const malayalamToManglish = (r) => {
-  const e = {
-    അ: "a",
-    ആ: "aa",
-    ഇ: "i",
-    ഈ: "ee",
-    ഉ: "u",
-    ഊ: "oo",
-    ഋ: "ru",
-    എ: "e",
-    ഏ: "e",
-    ഐ: "ai",
-    ഒ: "o",
-    ഓ: "o",
-    ഔ: "au",
-  };
+/**
+ * Converts Malayalam Script to Romanized Manglish
+ */
+const malayalamToManglish = (text) => {
+  if (!text) return "";
 
-  const a = {
-    ക്ക: "kk",
-    ഗ്ഗ: "gg",
-    ങ്ങ: "ng",
-    ക്ക: "kk",
-    ച്ച: "cch",
-    ജ്ജ: "jj",
-    ഞ്ഞ: "nj",
-    ട്ട: "tt",
-    ണ്ണ: "nn",
-    ത്ത: "tth",
-    ദ്ദ: "ddh",
-    ദ്ധ: "ddh",
-    ന്ന: "nn",
-    ന്ത: "nth",
-    ങ്ക: "nk",
-    ണ്ട: "nd",
-    ബ്ബ: "bb",
-    പ്പ: "pp",
-    മ്മ: "mm",
-    യ്യ: "yy",
-    ല്ല: "ll",
-    വ്വ: "vv",
-    ശ്ശ: "sh",
-    സ്സ: "s",
-    ക്സ: "ks",
-    ഞ്ച: "nch",
-    ക്ഷ: "ksh",
-    മ്പ: "mp",
-    റ്റ: "tt",
-    ന്റ: "nt",
-    ന്ത: "nth",
-    ന്ത്യ: "nthy",
-  };
+  // Character Mappings (Vowels, Consonants, Modifiers)
+  const vowels = { "അ": "a", "ആ": "aa", "ഇ": "i", "ഈ": "ee", "ഉ": "u", "ഊ": "oo", "ഋ": "ru", "എ": "e", "ഏ": "e", "ഐ": "ai", "ഒ": "o", "ഓ": "o", "ഔ": "au" };
+  const doubleConsonants = { "ക്ക": "kk", "ഗ്ഗ": "gg", "ങ്ങ": "ng", "ച്ച": "cch", "ജ്ജ": "jj", "ഞ്ഞ": "nj", "ട്ട": "tt", "ണ്ണ": "nn", "ത്ത": "tth", "ദ്ദ": "ddh", "ദ്ധ": "ddh", "ന്ന": "nn", "ന്ത": "nth", "ങ്ക": "nk", "ണ്ട": "nd", "ബ്ബ": "bb", "പ്പ": "pp", "മ്മ": "mm", "യ്യ": "yy", "ല്ല": "ll", "വ്വ": "vv", "ശ്ശ": "sh", "സ്സ": "s", "ക്സ": "ks", "ഞ്ച": "nch", "ക്ഷ": "ksh", "മ്പ": "mp", "റ്റ": "tt", "ന്റ": "nt", "ന്ത്യ": "nthy" };
+  const consonants = { "ക": "k", "ഖ": "kh", "ഗ": "g", "ഘ": "gh", "ങ": "ng", "ച": "ch", "ഛ": "chh", "ജ": "j", "ഝ": "jh", "ഞ": "nj", "ട": "d", "ഠ: "dh", "ഡ": "d", "ഢ": "dd", "ണ": "n", "ത": "th", "ഥ": "th", "ദ": "d", "ധ": "dh", "ന": "n", "പ": "p", "ഫ": "ph", "ബ": "b", "ഭ": "bh", "മ": "m", "യ": "y", "ര": "r", "ല": "l", "വ": "v", "ശ": "sh", "ഷ": "sh", "സ": "s", "ഹ": "h", "ള": "l", "ഴ: "zh", "റ": "r" };
+  const chills = { "ല്": "l", "ള്": "l", "ണ്": "n", "ന്": "n", "ര്": "r", "ക്ക്": "k" };
+  const modifiers = { "ു്": "u", "ാ": "aa", "ി": "i", "ീ": "ee", "ു": "u", "ൂ": "oo", "ൃ": "ru", "െ": "e", "േ": "e", "ൈ": "y", "ൊ": "o", "ോ": "o", "ൌ": "ou", "ൗ": "au", "ഃ": "a" };
 
-  const n = {
-    ക: "k",
-    ഖ: "kh",
-    ഗ: "g",
-    ഘ: "gh",
-    ങ: "ng",
-    ച: "ch",
-    ഛ: "chh",
-    ജ: "j",
-    ഝ: "jh",
-    ഞ: "nj",
-    ട: "d",
-    ഠ: "dh",
-    ഡ: "d",
-    ഢ: "dd",
-    ണ: "n",
-    ത: "th",
-    ഥ: "th",
-    ദ: "d",
-    ധ: "dh",
-    ന: "n",
-    പ: "p",
-    ഫ: "ph",
-    ബ: "b",
-    ഭ: "bh",
-    മ: "m",
-    യ: "y",
-    ര: "r",
-    ല: "l",
-    വ: "v",
-    ശ: "sh",
-    ഷ: "sh",
-    സ: "s",
-    ഹ: "h",
-    ള: "l",
-    ഴ: "zh",
-    റ: "r",
-  };
+  let result = text.replace(/[\u200B-\u200D\uFEFF]/g, ""); // Clean hidden characters
 
-  const t = {
-    ല്: "l",
-    ള്: "l",
-    ണ്: "n",
-    ന്: "n",
-    ര്: "r",
-    ക്ക്: "k",
-  };
+  // Translation Logic
+  Object.keys(doubleConsonants).forEach(k => result = result.split(k).join(doubleConsonants[k] + "a"));
+  Object.keys(consonants).forEach(k => result = result.split(k).join(consonants[k] + "a"));
+  Object.keys(vowels).forEach(k => result = result.split(k).join(vowels[k]));
+  Object.keys(modifiers).forEach(k => result = result.split(k).join(modifiers[k]));
+  result = result.replace(/ം/g, "m");
 
-  const o = {
-    "ു്": "u",
-    "ാ": "aa",
-    "ി": "i",
-    "ീ": "ee",
-    "ു": "u",
-    "ൂ": "oo",
-    "ൃ": "ru",
-    "െ": "e",
-    "േ": "e",
-    "ൈ": "y",
-    "ൊ": "o",
-    "ോ": "o",
-    "ൌ": "ou",
-    "ൗ": "au",
-    "ഃ": "a",
-  };
-
-  function h(r) {
-    r = p(a, (r = r.replace(/[\u200B-\u200D\uFEFF]/g, "")));
-    r = p(e, r);
-    r = p(n, r);
-    let h = "";
-
-    for (const l in a) {
-      if (a.hasOwnProperty(l)) {
-        h = a[l];
-        r = (r = (r = r.replace(RegExp(l + "്([\\w])", "g"), h + "$1")).replace(
-          RegExp(l + "്", "g"),
-          h + "u"
-        )).replace(RegExp(l, "g"), h + "a");
-      }
-    }
-
-    for (const l in n) {
-      if (n.hasOwnProperty(l)) {
-        h = n[l];
-        r = r.replace(RegExp(l + "(?!്)", "g"), h + "a");
-      }
-    }
-
-    for (const l in n) {
-      if (n.hasOwnProperty(l)) {
-        h = n[l];
-        r = r.replace(RegExp(l + "്(?![\\s).;,\"'/\\%!])", "ig"), h);
-      }
-    }
-
-    for (const l in n) {
-      if (n.hasOwnProperty(l)) {
-        h = n[l];
-        r = r.replace(RegExp(l + "്", "g"), h + "u");
-      }
-    }
-
-    for (const l in n) {
-      if (n.hasOwnProperty(l)) {
-        h = n[l];
-        r = r.replace(RegExp(l, "g"), h);
-      }
-    }
-
-    for (const l in e) {
-      if (e.hasOwnProperty(l)) {
-        h = e[l];
-        r = r.replace(RegExp(l, "g"), h);
-      }
-    }
-
-    for (const l in t) {
-      if (t.hasOwnProperty(l)) {
-        h = t[l];
-        r = r.replace(RegExp(l, "g"), h);
-      }
-    }
-
-    r = r.replace(/ം/g, "m");
-
-    for (const l in o) {
-      if (o.hasOwnProperty(l)) {
-        h = o[l];
-        r = r.replace(RegExp(l, "g"), h);
-      }
-    }
-
-    return r.replace(/(^\s*\w|[\.\!\?]\s*\w)/g, function (r) {
-      return r.toUpperCase();
-    });
-  }
-
-  function p(r, e) {
-    for (
-      let a = 0,
-        n = RegExp("(" + l(r).join("|") + ")(" + l(o).join("|") + ")", "g");
-      null != a;
-
-    ) {
-      (a = n.exec(e)) && (e = e.replace(RegExp(a[0], "g"), r[a[1]] + o[a[2]]));
-    }
-    return e;
-  }
-
-  function l(r) {
-    const e = [];
-    for (const a in r) {
-      r.hasOwnProperty(a) && e.push(a);
-    }
-    return e;
-  }
-
-  return h(r);
+  return result.trim();
 };
 
-const manglishToMalayalam = async (r) => {
+/**
+ * Converts Manglish (Roman) to Malayalam Script via Google API
+ */
+const manglishToMalayalam = async (query) => {
   try {
-    const { data: e } = await axios.get(
-      `https://inputtools.google.com/request?text=${r}&itc=ml-t-i0-und&num=5&cp=0&cs=1&ie=utf-8&oe=utf-8`
-    );
-    return e[1][0][1][0] || false;
-  } catch (a) {
-    console.log(a.message);
+    const url = `https://inputtools.google.com/request?text=${encodeURIComponent(query)}&itc=ml-t-i0-und&num=5&cp=0&cs=1&ie=utf-8&oe=utf-8`;
+    const { data } = await axios.get(url);
+    if (data && data[0] === "SUCCESS") {
+        return data[1][0][1][0];
+    }
+    return false;
+  } catch (error) {
+    console.error("Manglish Error:", error.message);
     return false;
   }
 };
 
-module.exports = {
-  malayalamToManglish,
-  manglishToMalayalam,
-};
+module.exports = { malayalamToManglish, manglishToMalayalam };
+  
